6b8f59f88f2452c74bbdbef15bf964e7884518a1
skmojahidulislam <skmojahidulislam298@gmail.com>
2025-07-17 11:32:33 +0530

    Added Express API project with README

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.json(
+        [
+            {
+              id: 1,
+              name: "Mujahid",
+              Salary: 60000,
+            },
+
+            {
+              id: 2,
+              name: "Huzaifa",
+              salary: 70000
+            },
+
+            {
+              id: 3,
+              name: "Abdul",
+              salary: 50000
+            },
+        ]
+    )
+});
+
+app.listen(port, () => {
+    console.log("Server is running port: 3000")
+})
